841ffc29f64a5c9dec6e8d8e63688ac476c2232b
kyjna0312 <kyjna0312@gmail.com>
2021-01-07 15:22:29 +0900

    Create Parsing_Message.java
    for message

---
+++ java/Parsing_Message.java
@@ -0,0 +1,13 @@
+package com.example.demoproject;
+
+public class Parsing_Message {
+    String message;
+
+    public void set_Message(String str){
+        message = str;
+    }
+
+    public String get_Message(){
+        return message;
+    }
+}
